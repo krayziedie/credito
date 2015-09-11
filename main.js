4d7746eeb9f2d40ddf7d7b52f3ce855c3a4b0a84
krayziedie <diego.aguileram@gmail.com>
2015-09-10 22:08:38 -0300

    se traen datos por ajax y se muestran valores y calculos de UF

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-
-
+//$(document).ready(function($){
+$('#btn').click(function(e){
+	e.preventDefault();
+	var monto = $('#monto').val();	
+	var cuotas = $('#cuotas').val();
+	var $conv = $('.conversion');
 $.ajax({
 	url: 'http://www.mindicador.cl/api',
-		dataType:'json',
-})
-.done(function(data, textStatus){
-	console.log(data.uf);
-	console.log(textStatus);
-})
+	dataType:'json',
+	})
+.done(function(data){
+	var html='<div class="conversion">';
+	html += '<p>valor de la cuota '+(monto / cuotas) / data.uf.valor+'</p>';
+	//html += '<p>UF: '+monto / data.uf.valor+'</p>';
+	//html += '<p>US$: '+monto / data.dolar.valor+'</p>';
+	html += '</div>';
+	//console.log(data);
+	//console.log(textStatus);
+	$conv.append(html);
+	})
 .fail(function(err) {
 	console.log(errorThrown, textStatus);
-})
-
-
+	})
+});
 
 
